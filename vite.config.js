748f25b9668176b4fbadcc1a283ac70bc5fb6c2f
Amir-Afkir <afkir202@gmail.com>
2026-09-05 16:16:52 +0200

    fix vite config conflict

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite';
-<<<<<<< HEAD
-
-export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/Zerana/' : '/',
-=======
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true
-  }
->>>>>>> 541ee88 ( batiment + arbre ajouté)
+  base: process.env.GITHUB_ACTIONS ? '/Zerana/' : '/',
+  server: { open: true },
 });
